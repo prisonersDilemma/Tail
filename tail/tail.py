af72fcae8154e454b506504590a4eab0fc5dbf38
GH05T <outoftheshadowsintotheabyss@gmail.com>
2017-11-30 23:32:05 -0500

    (Last push didn't contain lastest changes) Refactoring: Trying to remove StopIteration and 'push' up StopIteration raised form NegativeOffset.

--- tail/tail.py
+++ tail/tail.py
@@ -2,7 +2,7 @@
 """Incrementally "walk" backwards through a file, reading "blocks" of bufsz,
 yielding chunks of nlines."""
 __date__ = '2017-11-22'
-__version__ = (0,2,1)
+__version__ = (0,3,1)
 from codecs import lookup
 from os import getcwd
 from os import SEEK_CUR
@@ -16,7 +16,7 @@ import yyyymmdd
 
 from logging import basicConfig, getLogger
 
-basicConfig(filename=f'{getcwd()}/tail-dev.log', filemode='w',)
+basicConfig(filename=f'{getcwd()}/tail.log', filemode='w',)
 logger = getLogger('Tail')
 logger.setLevel('DEBUG') # str -> int
 
@@ -70,78 +70,72 @@ class Tail:
         return self
 
 
-    # make sure we can handle nlines > lines in file
     def __next__(self):
         """Return a 'chunk' of nlines as a list of decoded strings, without newlines."""
+        #if self.buffer is None:
+        #    #logger.debug('Raising StopIteration.')
+        #    raise StopIteration
 
-        if self.buffer is None:
-            #logger.debug('Raising StopIteration.')
-            raise StopIteration
 
-
-        try: # Raises StopIteration for us.
-            next(self.pos)
-        except StopIteration:
-            logger.debug('self.pos raised StopIteration. Buffer still has len %s. Emptying the buffer.', len(self.buffer))
+        #try: # Raises StopIteration for us.
+        next(self.pos)
+        if self.pos.prev is 0: #empty_buffer or next iter? may have one last read.
+        #except StopIteration:
+        #   logger.debug('StopIteration raised by self.pos %r.', self.pos)
+        #   logger.debug('Buffer still has len %s. Emptying the buffer.', len(self.buffer))
             # Return what remains in the buffer.
+            #print('Emptying buffer.')
             return self.empty_buffer()
 
-
-        #logger.debug('%r\n', self)
         self.stream.seek(self.pos.crnt)
 
+        chunk = self.stream.read(self.pos._delta())
 
         # Reading the file in reverse, so prepend new data.
-        delta = self.pos._delta()
-        #logger.debug('delta: %s', delta)
-
-        #logger.debug('Reading pos %r delta %s', self.pos, delta)
-        _delta = self.stream.read(delta)
-
-        #logger.debug('%s bytes read. Prepending to (%s) to buffer: (%s)', self.pos._delta(), _delta, self.buffer)
-        self.buffer = _delta + self.buffer
-        logger.debug('Buffer + %s bytes read: %s', self.pos._delta(), self.buffer)
+        self.buffer = chunk + self.buffer
+        #logger.debug('Buffer + %s bytes read: %s', self.pos._delta(), self.buffer)
 
 
-        # It's our first read (from the end of the file to bufsz). Strip final
-        # newline in file.
+        # Strip the final newline from file if it's our first read.
         if self.pos.prev is self._stat.st_size:
             self.buffer = self.buffer.rstrip(self.newline)
 
 
-        # Try to take an extra line to get rid of fragments. But will consume all
-        # the lines if there are too few. Take nlines and put the extra back in
-        # the buffer b/c it's probably a fragment.
-
+        # Split an extra line so we can save it for later b/c it's probably
+        # a fragment. Consume all the lines if nlines+1 are NOT present.
         cache = self.buffer.rsplit(self.newline, self.nlines+1)
-        self.lines_cache.lextend(cache[1:]) # prepend the lines
-        self.buffer = cache[0]
-        logger.debug('lines_cache (len(%s)) after rsplit: %s', len(self.lines_cache), self.lines_cache)
+        self.lines_cache.lextend(cache[1:]) # "prepend" to SuperList
+        self.buffer = cache[0] # Everything remaining beyond nlines+1.
+        #logger.debug('lines_cache (len(%s)) after rsplit: %s', len(self.lines_cache), self.lines_cache)
 
 
+        # Recurse if we don't have enough lines, yet.
         if len(self.lines_cache) < (self.nlines):
-            logger.debug('Lines_cache (chunk) len (%s) < (%s). Recursing!\n', len(self.lines_cache), self.nlines)
+            #logger.debug('Lines_cache (chunk) len (%s) < (%s). Recursing!\n', len(self.lines_cache), self.nlines)
             return next(self)
-        else:
+        else: # Shouldn't need an explicit 'else' here.
             nlines = self.lines_cache.rsplit(-self.nlines) # idx at 10 lines
-            logger.debug('Remaining lines_cache len(%s): %s', len(self.lines_cache), self.lines_cache)
-            logger.debug('Returning nlines (%s): %s.\n\n', len(nlines), nlines)
-            chunk = self.newline.join(nlines).decode(self.encoding).splitlines() # Faster than listcomp! I timed it. decoding is expensive!
-            return chunk
-
-
+            #logger.debug('Remaining lines_cache len(%s): %s', len(self.lines_cache), self.lines_cache)
+            #logger.debug('Returning nlines (%s): %s.\n\n', len(nlines), nlines)
+            return self.newline.join(nlines).decode(self.encoding).splitlines() # Faster than listcomp! I timed it. decoding is expensive!
 
 
     def empty_buffer(self):
+        self.stream.seek(self.pos.crnt)
+        chunk = self.stream.read(self.pos._delta())
+        #chunk = chunk if chunk is not None else b''
+        #self.buffer = self.buffer if self.buffer is not None else b''
+        #print(type(chunk))
+        #print(type(self.buffer))
+        self.buffer = chunk + self.buffer
+
         self.lines_cache.lextend(self.buffer.splitlines())
-        nlines = self.lines_cache[:] # copy
+        nlines = self.lines_cache[:] # Copy. Don't mutate the list!
         chunk = self.newline.join(nlines).decode(self.encoding).splitlines()
         logger.debug('Returning final chunk of len %s', len(chunk))
         self.lines_cache = self.buffer = None
         return chunk
 
-# I wonder if the recursion is causing chunks to be reconstructed out of order?
-# But still don't know how to account for fragments.
 
     def stop(self):
         """Close the stream opened during instantiation at *fpath*."""
@@ -162,11 +156,10 @@ if __name__ == '__main__':
 
 
 #===============================================================================
-# Removed assertions in __init__ for better tests and error messages.
-# Added a test for codec name checking (e.g., the value passed for *encoding*).
-# Changed newline parameter to take type str, which is then converted to binary
-# during construction.
-# Added an explict 'stop' method, which closes the stream at *fpath*.
-# Added fix for AttributeError, raised when trying to close a stream if it
-#   was never opened in the first place, in __del__.
-#===============================================================================
+# Changelog:
+#
+# Notes:
+# Appears to handle nlines > lines in file.
+#
+# Todo:
+#
